1e6069cd3cdabdc9814a6d34bbaff4b57fb36d43
Ramya Sarma <rsarma92@gmail.com>
2015-09-06 16:28:55 +0530

    Changed UI to tabbed navigation

--- app/src/main/java/com/garamtech/manipuribible/activity/BooksActivity.java
+++ app/src/main/java/com/garamtech/manipuribible/activity/BooksActivity.java
@@ -1,103 +1,70 @@
 package com.garamtech.manipuribible.activity;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.garamtech.manipuribible.R;
 
 
-public class BooksActivity extends Activity {
+public class BooksActivity extends AppCompatActivity {
+
+    // Declaring Your View and Variables
+
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Kitepna Lui","Kitepna Thah"};
+    int Numboftabs =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
-        final ListView listView = (ListView) findViewById(R.id.listView);
 
-        // Defined Array values to show in ListView
-        String[] values = new String[] {
-                "Kitepna Lui","Kitepna Thah"
 
-        };
+        // Creating The Toolbar and setting it as the Toolbar for the activity
 
-        // Define a new Adapter
-        // First parameter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values) {
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                TextView text = (TextView) view.findViewById(android.R.id.text1);
-                text.setTextColor(Color.WHITE);
-                return view;
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
             }
-        };
-
-
-        // Assign adapter to ListView
-        listView.setAdapter(adapter);
-
-        // ListView Item Click Listener
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                // ListView Clicked item index
-                int itemPosition     = position;
-
-                // ListView Clicked item value
-                String  itemValue    = (String) listView.getItemAtPosition(position);
-                if(itemPosition==0){ //The New Testament
-
-                    Context context = getApplicationContext();
-                    CharSequence text = "1";
-                    int duration = Toast.LENGTH_SHORT;
-
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                   Intent intent = new Intent(BooksActivity.this, NewTestamentActivity.class);
-                    BooksActivity.this.startActivity(intent);
-
-                }
-                else{//The Old Testament
-                    Context context = getApplicationContext();
-                    CharSequence text = "2";
-                    int duration = Toast.LENGTH_SHORT;
-
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                    Intent intent = new Intent(BooksActivity.this, OldTestamentActivity.class);
-                    BooksActivity.this.startActivity(intent);
-
-                }
-
-            }
-
         });
 
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+
+
+
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_books, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -116,4 +83,3 @@ public class BooksActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
-
--- app/src/main/java/com/garamtech/manipuribible/activity/MainActivity.java
+++ app/src/main/java/com/garamtech/manipuribible/activity/MainActivity.java
@@ -1,8 +1,7 @@
 package com.garamtech.manipuribible.activity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,12 +9,15 @@ import android.widget.Button;
 
 import com.garamtech.manipuribible.R;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
+
     }
 
     @Override
